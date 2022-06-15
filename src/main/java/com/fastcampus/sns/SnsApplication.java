7bccd0ae8b671a509fdf39afd7623d0eb87518d6
KimHyoJin <hyojin03131995@gmail.com>
2022-06-15 11:11:43 +0900

    init : initialize project

---
+++ src/main/java/com/fastcampus/sns/SnsApplication.java
@@ -0,0 +1,14 @@
+package com.fastcampus.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
